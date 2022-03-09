9a7a147fe155bd32cbe58ad6e2d2f583f6a01b5e
Arman Rafian <arafian@calpoly.edu>
2022-03-09 11:09:54 -0800

    Add isStop, isPush, and isRule

--- agents/helpers/predicates.js
+++ agents/helpers/predicates.js
@@ -71,6 +71,58 @@ function isWin(state, wins) {
 
 }
 
+/**
+ * @description Filter all of the objects that are STOP in the current game state.
+ *              If stops is empty, all of the objects that are STOP in the current state.
+ * @param {string} state the acsii representation of the current game state.
+ * @param {array} stops possible values of stop to filter OR an empty array.
+ * @return {array} all objects in stops that are STOP - or all of them - in the current game state.
+ */
+ function isStop(state, stops) {
+  const stoppables = accessGameState(state, "stoppables");
 
+  if (stops.length > 0) {
+    stops = stops.filter((s) => stoppables.includes(s));
+  } else {
+    stops = stoppables;
+  }
+  return stops;
+}
 
-module.exports = { isYou, isWin, isReachable };
+/**
+ * @description Filter all of the objects that are PUSH in the current game state.
+ *              If pushes is empty, all of the objects that are PUSH in the current state.
+ * @param {string} state the acsii representation of the current game state.
+ * @param {array} pushes possible values of push to filter OR an empty array.
+ * @return {array} all objects in pushes that are PUSH - or all of them - in the current game state.
+ */
+ function isPush(state, pushes) {
+  const pushables = accessGameState(state, "pushables");
+
+  if (pushes.length > 0) {
+    pushes = pushes.filter((p) => pushables.includes(p));
+  } else {
+    pushes = pushables;
+  }
+  return pushes;
+}
+
+/**
+ * @description Filter all of the objects that are RULE in the current game state.
+ *              If rules is empty, all of the objects that are RULE in the current state.
+ * @param {string} state the acsii representation of the current game state.
+ * @param {array} rules possible values of rule to filter OR an empty array.
+ * @return {array} all objects in rules that are RULE - or all of them - in the current game state.
+ */
+ function isRule(state, rules) {
+  const state_rules = accessGameState(state, "rules");
+
+  if (rules.length > 0) {
+    rules = rules.filter((r) => state_rules.includes(r));
+  } else {
+    rules = state_rules;
+  }
+  return rules;
+}
+
+module.exports = { isYou, isWin, isReachable, isStop, isPush, isRule };
