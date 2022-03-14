449cc66a1fb6e4495f2b09abefac8308ad5f59ba
Greg <person3222@gmail.com>
2022-03-13 17:49:34 -0700

    Added assertWin() and createWin() predicates to predicates.js. Updated rules.js to export some functions. Needs testing.

--- agents/helpers/predicates.js
+++ agents/helpers/predicates.js
@@ -1,6 +1,7 @@
 const { nextMove } = require("../../js/simulation");
 const { accessGameState, deepCopy, deepCopyObject, copy_state, Position, add_to_dict, permutations_of_list, static, simulate } = require("./helpers");
 const { floodfill_reachable, a_star_reachable, game_bound_check, a_star_avoid_push, a_star_pushing } = require("./pathing");
+const { generateRules, generatePropertyRules, generateNounRules, canChangeRules, canActivateRules, activeRules, getRules } = require("./rules");
 
 const simjs = require("../../js/simulation");
 
@@ -793,6 +794,62 @@ function isProperty(state, properties) {
     return properties;
 }
 
+/**
+ * @description Find all possible "win" rules that the agent is able to activate.
+ *              If passed a non-empty list of nouns, it will only find "win" rules using those nouns
+ * @param {string} state the acsii representation of the current game state.
+ * @param {array} nouns a set of noun words in the game state OR an empty array.
+ * @return {array} a list of rules and their paths of activation, as created by canActivateRules.
+ *
+ */
+function assertWin(state, nouns) {
+    let property_rules = [];
+    if (nouns.length == 0) {
+        property_rules = generatePropertyRules(state, [], []);
+    } else {
+        let win_words = isProperty(state, []).filter((p) => p.name = "win");
+        let connectors = isConnector(state, []);
+        property_rules = generatePropertyRules(state, nouns.concat(win_words, connectors));
+    }
+
+    let win_rules = property_rules.filter((r) => r.property.name = "win");
+    return canActivateRules(state, win_rules);
+}
+
+/**
+ * @description Find all possible rules that change one object into another object type that is already "win" that the agent is able to activate.
+ *              If passed a non-empty list of subject_nouns, it will only find rules using those nouns AS THE SUBJECT(the thing being transformed)
+ * @param {string} state the acsii representation of the current game state.
+ * @param {array} subject_nouns a set of subject noun words in the game state OR an empty array.
+ * @return {array} a list of rules and their paths of activation, as created by canActivateRules.
+ *
+ */
+function createWin(state, subject_nouns) {
+    let win_rules = activeRules(state, []).filter((r) => r.property.name = "win");
+    let win_nouns = []
+    let win_connectors = []
+    let has_win_property = []
+    for (rule of win_rules) {
+        win_nouns.push(rule.noun);
+        win_connectors.push(rule.connector)
+        has_win_property.push(rule.noun.name);
+    }
+
+    //get a list of usable subject nouns
+    let usable_subjects = [];
+    if (subject_nouns.length == 0) {
+        usable_subjects = isNoun(state, []).filter((n) => !win_nouns.includes(n.name));
+    } else {
+        usable_subjects = subject_nouns;
+    }
+
+    //filter any connector and noun object that has win property but is not part of a win rule
+    let usable_connectors = isConnector(state, []).filter((c) => !win_connectors.includes(c));
+    let usable_win_nouns = isNoun(state, []).filter((n) => !win_nouns.includes(n) && has_win_property.includes(n.name));
+    let possible_rules = getRules(usable_subjects, usable_connectors, usable_win_nouns);
+    return canActivateRules(state, possible_rules);
+}
+
 module.exports = {
     isYou,
     isWin,
@@ -810,5 +867,6 @@ module.exports = {
     isNoun,
     isConnector,
     isProperty,
-    canPushThese
+    assertWin,
+    createWin
 };
